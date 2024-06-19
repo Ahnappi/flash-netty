dc64f0f8cef1fc4a5aee5d2994e39eaf7c9143dc
ahnappi <lxy18735996526@163.com>
2024-06-19 14:09:21 +0800

    注释

--- src/main/java/the/flash/NettyServer.java
+++ src/main/java/the/flash/NettyServer.java
@@ -8,6 +8,12 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.AttributeKey;
 
+/**
+ * Netty 服务端启动的流程，一句话来说就是：创建一个引导类，然后给他指定线程模型，IO模型，连接读写处理逻辑，绑定端口之后，服务端就启动起来了。
+ * bind 方法是异步的，我们可以通过这个异步机制来实现端口递增绑定。
+ *
+ */
+
 public class NettyServer {
 
     private static final int BEGIN_PORT = 8000;
@@ -19,14 +25,31 @@ public class NettyServer {
         final ServerBootstrap serverBootstrap = new ServerBootstrap();
         final AttributeKey<Object> clientKey = AttributeKey.newInstance("clientKey");
         serverBootstrap
+                // 给引导类配置两大线程组
                 .group(boosGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
+                /**
+                 * 给服务端的 channel，也就是NioServerSocketChannel指定一些自定义属性，然后我们可以通过channel.attr()取出这个属性，
+                 * 比如，上面的代码我们指定我们服务端channel的一个serverName属性，属性值为nettyServer，
+                 * 其实说白了就是给NioServerSocketChannel维护一个map而已，通常情况下，我们也用不上这个方法。
+                 */
                 .attr(AttributeKey.newInstance("serverName"), "nettyServer")
+                // 可以给每一条连接指定自定义属性，然后后续我们可以通过channel.attr()取出该属性。
                 .childAttr(clientKey, "clientValue")
+                // 我们还可以给服务端channel设置一些属性，最常见的就是so_backlog，如下设置,表示系统用于临时存放已完成三次握手的请求的队列的最大长度，如果连接建立频繁，服务器处理创建新连接较慢，可以适当调大这个参数
                 .option(ChannelOption.SO_BACKLOG, 1024)
+                /**
+                 * childOption()可以给每条连接设置一些TCP底层相关的属性，比如上面，我们设置了两种TCP属性，其中
+                 *
+                 * ChannelOption.SO_KEEPALIVE表示是否开启TCP底层心跳机制，true为开启
+                 * ChannelOption.TCP_NODELAY表示是否开启Nagle算法，true表示关闭，false表示开启，通俗地说，如果要求高实时性，
+                 *              有数据发送时就马上发送，就关闭，如果需要减少发送次数减少网络交互，就开启。
+                 */
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
                 .childOption(ChannelOption.TCP_NODELAY, true)
+                // 定义后续每条连接的数据读写，业务处理逻辑
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
                     protected void initChannel(NioSocketChannel ch) {
                         System.out.println(ch.attr(clientKey).get());
                     }
